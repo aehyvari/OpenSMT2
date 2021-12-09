d34565557e676ea3205e3d86884f387c0989484e
Antti <antti.hyvarinen@gmail.com>
2021-12-09 11:21:08 -0800

    fsbv: Renamed functions, implemented some bit blasting.

--- src/logics/FSBVBitBlaster.h
+++ src/logics/FSBVBitBlaster.h
@@ -5,4 +5,43 @@
 #ifndef OPENSMT_FSBVBITBLASTER_H
 #define OPENSMT_FSBVBITBLASTER_H
 
+#include "FSBVLogic.h"
+#include "bvsolver/Bvector.h"
+#include "bvsolver/BVStore.h"
+#include "OsmtInternalException.h"
+
+class FSBVBitBlaster {
+    FSBVLogic & logic;
+    BVStore bs;
+
+    static constexpr const char * s_add = "add";
+    static constexpr const char * s_mul = "mul";
+    static constexpr const char * s_ule = "ule";
+
+    // Predicates
+    PTRef bbUlt(PTRef tr);
+
+    void notImplemented(PTRef tr) { throw OsmtInternalException(std::string("Not implemented: ") + logic.getSymName(tr)); }
+    BVRef bbConcat(PTRef tr) { notImplemented(tr); return BVRef_Undef; }
+    BVRef bbNot(PTRef tr) { notImplemented(tr); return BVRef_Undef; }
+    BVRef bbNeg(PTRef tr) { notImplemented(tr); return BVRef_Undef; }
+    BVRef bbAnd(PTRef tr) { notImplemented(tr); return BVRef_Undef; }
+    BVRef bbOr(PTRef tr) { notImplemented(tr); return BVRef_Undef; }
+    BVRef bbAdd(PTRef tr);
+    BVRef bbMul(PTRef tr);
+    BVRef bbUdiv(PTRef tr) { notImplemented(tr); return BVRef_Undef; }
+    BVRef bbUrem(PTRef tr) { notImplemented(tr); return BVRef_Undef; }
+    BVRef bbShl(PTRef tr) { notImplemented(tr); return BVRef_Undef; }
+    BVRef bbLshr(PTRef tr) { notImplemented(tr); return BVRef_Undef; }
+    BVRef bbConstant(PTRef tr);
+    BVRef bbVar(PTRef);
+
+    vec<PTRef> getBVVars(std::string const & base, BitWidth_t width);
+public:
+    FSBVBitBlaster(FSBVLogic & fsbvLogic) : logic(fsbvLogic) { }
+
+    PTRef bbPredicate(PTRef);
+
+    BVRef bbTerm(PTRef);
+};
 #endif //OPENSMT_FSBVBITBLASTER_H
--- src/logics/FSBVTheory.cc
+++ src/logics/FSBVTheory.cc
@@ -1,4 +1,36 @@
-//
-// Created by prova on 08.12.21.
-//
+/*
+ * Copyright (c) 2021, Antti Hyvarinen <antti.hyvarinen@gmail.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
 
+#include "FSBVTheory.h"
+#include "OsmtInternalException.h"
+#include "FSBVBitBlaster.h"
+#include "TreeOps.h"
+
+static SolverDescr descr_bb_solver("BitBlaster", "BitBlaster for counting models?");
+
+bool FSBVTheory::simplify(vec<PFRef> const & formulas, PartitionManager & pmanager, int curr) {
+    if (keepPartitions()) {
+        throw OsmtInternalException("Mode not supported for QF_BV yet");
+    } else {
+
+        PTRef coll_f = getCollateFunction(formulas, curr);
+        PTRef trans = getLogic().learnEqTransitivity(coll_f);
+        coll_f = getLogic().mkAnd(coll_f, trans);
+        auto subs_res = computeSubstitutions(coll_f);
+        PTRef fla = flaFromSubstitutionResult(subs_res);
+
+        vec<PTRef> bvFormulas;
+        FSBVBitBlaster bitBlaster(logic);
+        topLevelConjuncts(logic, fla, bvFormulas);
+        for (PTRef tr : bvFormulas) {
+            if (logic.isBoolAtom(tr)) continue;
+            std::cout << logic.pp(tr) << std::endl;
+            PTRef out = bitBlaster.bbPredicate(tr);
+            std::cout << logic.pp(out) << std::endl;
+        }
+        return false;
+    }
+}
