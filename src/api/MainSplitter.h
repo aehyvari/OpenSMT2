e04485bc132c4291ea92a1e1f2b65ce7b8a77ab3
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2021-12-20 14:10:56 +0100

    Add createInnerSolver for splitters and getPartitionSplits

--- src/api/MainSplitter.h
+++ src/api/MainSplitter.h
@@ -6,12 +6,29 @@
 #define OPENSMT_MAINSPLITTER_H
 
 #include "MainSolver.h"
+#include "ScatterSplitter.h"
 
 class MainSplitter : public MainSolver {
-public:
-    MainSplitter(Logic& logic, SMTConfig& config, std::string name) : MainSolver(logic, config, std::move(name)) {}
-    bool writeSolverSplits_smtlib2(const char* file, char** msg) const;
-};
 
+public:
+    static std::unique_ptr<SimpSMTSolver> createInnerSolver(SMTConfig& config, THandler& thandler, Channel& ch);
+    MainSplitter(std::unique_ptr<Theory> t,std::unique_ptr<TermMapper> tm, std::unique_ptr<THandler> th,
+                 std::unique_ptr<SimpSMTSolver> ss, Logic& logic, SMTConfig& config, std::string name)
+                 :
+                 MainSolver(
+                    std::move(t),
+                    std::move(tm),
+                    std::move(th),
+                    std::move(ss),
+                    logic,
+                    config,
+                    std::move(name)
+                 ){}
+
+    bool writeSolverSplits_smtlib2(const char* file, char** msg) const;
+
+    std::vector<std::string> getPartitionSplits();
+
+};
 
 #endif //OPENSMT_MAINSPLITTER_H
