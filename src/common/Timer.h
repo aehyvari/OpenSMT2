ded477a483f8c0739858f5be071cdf71394c99d2
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2021-12-20 13:58:15 +0100

    Add Stoppable Watch

--- src/common/Timer.h
+++ src/common/Timer.h
@@ -105,6 +105,87 @@ public:
     }
 };
 
+class StoppableWatch {
+
+    public:
+
+        StoppableWatch(const StoppableWatch& other) = default;
+
+        StoppableWatch(StoppableWatch&& other) = default;
+
+        virtual ~StoppableWatch() =default;
+
+        StoppableWatch& operator=(const StoppableWatch& other) = default;
+
+        StoppableWatch& operator=(StoppableWatch&& other) = default;
+
+        inline StoppableWatch(bool start =false) :
+                started_(false), paused_(false),
+                reference_(std::chrono::steady_clock::now()),
+                accumulated_(std::chrono::duration<long double>(0)) {
+            if (start)
+                this->start();
+        }
+        inline void start() {
+            if (!started_) {
+                started_ = true;
+                paused_ = false;
+                accumulated_ = std::chrono::duration<long double>(0);
+                reference_ = std::chrono::steady_clock::now();
+            } else if (paused_) {
+                reference_ = std::chrono::steady_clock::now();
+                paused_ = false;
+            }
+        }
+
+        inline void stop() {
+            if (started_ && !paused_) {
+                std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+                accumulated_ = accumulated_ + std::chrono::duration_cast< std::chrono::duration<long double> >(now - reference_);
+                paused_ = true;
+            }
+        }
+
+        inline void reset() {
+            if (started_) {
+                started_ = false;
+                paused_ = false;
+                reference_ = std::chrono::steady_clock::now();
+                accumulated_ = std::chrono::duration<long double>(0);
+            }
+        }
+
+        inline std::uint_fast16_t elapsed_time_milliseconds()
+        {
+            return count_elapsed<std::chrono::milliseconds>();
+        }
+
+        inline  std::uint_fast8_t elapsed_time_second()
+        {
+            return count_elapsed<std::chrono::seconds>();
+        }
+
+        template <class duration_t>
+        typename duration_t::rep count_elapsed() const {
+            if (started_) {
+                if (paused_) {
+                    return std::chrono::duration_cast<duration_t>(accumulated_).count();
+                } else {
+                    return std::chrono::duration_cast<duration_t>(
+                            accumulated_ + (std::chrono::steady_clock::now() - reference_)).count();
+                }
+            } else {
+                return duration_t(0).count();
+            }
+        }
+
+    private:
+        bool started_;
+        bool paused_;
+        std::chrono::steady_clock::time_point reference_;
+        std::chrono::duration<long double> accumulated_;
+    };
+
 class StopWatch {
     protected:
         struct rusage tmp_rusage;
@@ -128,26 +209,6 @@ class StopWatch {
     }
 };
 
-class PrintStopWatch {
-    protected:
-        char* name;
-        OSMTTimeVal timer;
-        std::ostream& os;
-        StopWatch* watch;
-    public:
-        PrintStopWatch(const char* _name, std::ostream& os) : os(os), watch(new StopWatch(timer))
-        {
-            int size = strlen(_name);
-            name = (char*)malloc(size+1);
-            strcpy(name, _name);
-        }
-        ~PrintStopWatch()
-        {
-            delete watch;
-            os << "; " << name << " " << timer.getTime() << " s\n";
-            free(name);
-        }
-};
 
 }
 #endif
